0580fc9f9e1a03e3dcf68dbbe75d75a32db895bd
Himanshu Shekhar <43611225+Shekh1995@users.noreply.github.com>
2026-08-30 20:42:42 -0700

    Update main.tf

--- modules/autoscaling/main.tf
+++ modules/autoscaling/main.tf
@@ -14,10 +14,12 @@ data "cloudinit_config" "config" {
 
 data "aws_ami" "ubuntu" {
   most_recent = true
+
   filter {
     name   = "name"
-    values = [ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*]
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
   }
+
   owners = ["099720109477"]
 }
 
